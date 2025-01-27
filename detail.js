5bf78c70d34a73a116ab0e98ef3c6c23a3d53545
CHOI <cws98tiger@naver.com>
2025-01-27 17:39:59 +0900

    feat: event-bubling & dataset

--- detail.js
+++ detail.js
@@ -1,12 +1,18 @@
-let con = $(".tab-button").length;
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       탭열기(i);
+//     });
+// }
 
-for (let i = 0; i < con; i++) {
-  $(".tab-button")
-    .eq(i)
-    .on("click", function () {
-      $(".tab-button").removeClass("orange");
-      $(".tab-button").eq(i).addClass("orange");
-      $(".tab-content").removeClass("show");
-      $(".tab-content").eq(i).addClass("show");
-    });
+$(".list").click(function (e) {
+  탭열기(e.target.dataset.id);
+});
+
+function 탭열기(i) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(i).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(i).addClass("show");
 }
